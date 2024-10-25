f33e827621223bf0a9971caa8fc8c8673e8c0ca6
ranga rohan <uniquerohanranga@gmail.com>
2024-10-25 11:54:59 +0530

    animations

--- lib/account_type.dart
+++ lib/account_type.dart
@@ -3,6 +3,7 @@ import 'package:admin_er/event_screen.dart';
 import 'package:admin_er/fee_dues.dart';
 import 'package:admin_er/signup_screen.dart';
 import 'package:admin_er/teacher_create_account.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class AccountType extends StatefulWidget {
@@ -29,25 +30,28 @@ class _AccountTypeState extends State<AccountType> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => const SignupScreen()));
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 3,
+                    ZoomIn(
+                      duration: Duration(seconds: 2),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => const SignupScreen()));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 3,
+                            ),
                           ),
-                        ),
-                        height: 200,
-                        width: 200,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Image.asset('assets/download.png'),
+                          height: 200,
+                          width: 200,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset('assets/download.png'),
+                          ),
                         ),
                       ),
                     ),
@@ -55,72 +59,82 @@ class _AccountTypeState extends State<AccountType> {
                       '-----or-----',
                       style: TextStyle(color: Colors.black, fontSize: 40),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => const TeacherCreateAccount()));
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 3,
+                    ZoomIn(
+                      duration: Duration(seconds: 2),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) =>
+                                      const TeacherCreateAccount()));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 3,
+                            ),
+                          ),
+                          height: 200,
+                          width: 200,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset('assets/8065183.png'),
                           ),
                         ),
-                        height: 200,
-                        width: 200,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Image.asset('assets/8065183.png'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    FadeInRight(
+                      duration: Duration(seconds: 2),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EventManagement()),
+                          );
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.red),
+                        ),
+                        label: const Text(
+                          'Post events',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        icon: const Icon(
+                          Icons.event,
+                          color: Colors.black,
                         ),
                       ),
                     ),
                     const SizedBox(
-                      height: 30,
+                      height: 20,
                     ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => EventManagement()),
-                        );
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(Colors.red),
-                      ),
-                      label: const Text(
-                        'Post events',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      icon: const Icon(
-                        Icons.event,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SendFeeScreen()),
-                        );
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(Colors.red),
-                      ),
-                      label: const Text(
-                        'Fee Receipt',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      icon: const Icon(
-                        Icons.credit_score,
-                        color: Colors.black,
+                    FadeInLeft(
+                      duration: Duration(seconds: 2),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SendFeeScreen()),
+                          );
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.red),
+                        ),
+                        label: const Text(
+                          'Fee Receipt',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        icon: const Icon(
+                          Icons.credit_score,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ],
